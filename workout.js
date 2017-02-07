0ff82a715655e8064b6d12725a68d19de04141f6
JPrier <JoshKPrier@gmail.com>
2017-02-06 22:58:57 -0500

    Draft 2

--- workout.js
+++ workout.js
@@ -1,20 +1,30 @@
 Module.register("workout",{
 	defaults: {
-		text: "Todays Morning Exercise\n Pushups: "+pushups+"\n"+ "Situps: "+ situps
+		pushupMax: 30,
+		pushupMin: 15,
+		situpMax: 40,
+		situpMin: 15,
+		squatMax: 50,
+		squatMin: 25,
+		pushups: Math 
+		text: "Todays Morning Exercise\n"
 	},
 	start: function() {
+		var max = 30;
+		var min = 10;
 		var self = this;
 		setInterval(function() {
 			self.updateDom();
-		}, 86400);
+		}, 86400000);
 	},
-	var max = 30;
-	var min = 10;
 	getDom: function() {
-		var max += (1/7);
-		var min += (1/7);
+		var max = max + (1/7);
+		var min = min + (1/7);
 		var wrapper = document.createElement("div");
-		this.pushups = Math.floor((Math.random()*(max-min + 1)) + min)
+
+		this.pushups = Math.floor((Math.random()*(max-min + 1)) + min);
+		this.situps = Math.floor((Math.random()*(max-min + 1)) + min);
+		this.config.text += "Pushups: " + pushups.toString() + "\nSitups: " + situps.toString();
 		wrapper.innerHTML = this.config.text;
 		return wrapper;
 	}
